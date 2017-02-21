1e0502a68668b90e5b978a9e7ecfd3ff69a4759e
srikrish85 <srikrish85@gmail.com>
2017-02-21 14:50:40 +0530

    Sample File

---
+++ server.js
@@ -0,0 +1,23 @@
+// Load required packages
+var express = require('express');
+
+// Create our Express application
+var app = express();
+
+// Add static middleware
+app.use(express.static(__dirname + '/public'));
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+router.get('/', function(req, res) {
+  res.end('Twitatron');
+});
+
+// Register all our routes
+app.use(router);
+
+// Start the server
+app.listen(3000);
+
